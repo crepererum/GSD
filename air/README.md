6b6febfae8d54fcf55485219690ddc37eb022b2f
Marco Neumann <marco@crepererum.net>
2013-05-21 12:38:31 +0200

    Air quality data

---
+++ air/README.md
@@ -0,0 +1,15 @@
+Air Quality
+===========
+
+The converted data will contain air quality metrics (e.g. carbon monoxid level) from different locations for multiple times (dimensions = locations).
+
+Requirements
+------------
+ - Python
+
+Usage
+-----
+Download a data package from [EPA](http://www.epa.gov/ttn/airs/airsaqs/detaildata/downloadaqsdata.htm), extract the txt file and run the following command:
+
+	./convert.py path/to/input.txt path/to/output.txt
+
---
+++ air/convert.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import sys
+
+# parse args
+fnameIn = sys.argv[1]
+fnameOut = sys.argv[2]
+
+# data structures
+skip = -2
+map = {}
+positions = set()
+timestamps = set()
+
+# read data
+fIn = open(fnameIn)
+lines = fIn.readlines()
+fIn.close()
+for l in lines:
+	if skip < 0:
+		skip += 1
+	else:
+		raw = l.split("|")
+		if len(raw) == 28:
+				state = raw[2]
+				county = raw[3]
+				site = raw[4]
+				date = raw[10]
+				time = raw[11]
+				valueRaw = raw[12]
+
+				pos = state + county + site
+				positions.add(pos)
+
+				if len(valueRaw) > 0:
+					timestamp = date + time
+					timestamps.add(timestamp)
+					if not timestamp in map:
+						map[timestamp] = {}
+					map[timestamp][pos] = valueRaw
+
+# prepare output
+positions = sorted(positions)
+timestamps = sorted(timestamps)
+
+# output
+out = open(fnameOut, "w")
+for timestamp in timestamps:
+	# print only timestamps with enough data
+	if len(map[timestamp]) >= 0.75 * len(positions):
+		avg = 0.0
+		for pos in map[timestamp]:
+			avg += float(map[timestamp][pos])
+		avg /= len(map[timestamp])
+		avg = str(avg)
+
+		first = True
+		for pos in positions:
+			if first:
+				first = False
+			else:
+				out.write(" ")
+			if pos in map[timestamp]:
+				out.write(map[timestamp][pos])
+			else:
+				out.write(avg)
+		out.write("\n")
+out.close()
+
