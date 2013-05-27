3db9e30a52af6379901df320490842a544191b5e
Marco Neumann <marco@crepererum.net>
2013-05-27 23:06:37 +0200

    Audio: Bugfixes, dB scale

--- audio/analyze.jl
+++ audio/analyze.jl
@@ -6,6 +6,7 @@ const PART_SIZE = 4096
 const NORM_SIZE = 32
 const ALPHA = 0.54
 const BETA = 1 - ALPHA
+const EPSILON = 1e-10
 
 function calcWindow()
 	result = Array(Float64, PART_SIZE)
@@ -25,7 +26,7 @@ function analyze(fname)
 
 	combined = (channel1 + channel2) / 2.0
 	parts = int64(floor(length(combined) / PART_SIZE))
-	result = Array(Float64, PART_SIZE)
+	result = Array(Float64, int64(PART_SIZE / 2))
 	n = 0
 	for p = 1:parts
 		pstart = 1 + (p-1) * PART_SIZE
@@ -35,10 +36,12 @@ function analyze(fname)
 		# apply window function
 		part .* window
 
-		result += abs(fft(part, 1))
+		f = fft(part, 1)
+		result += 20 .* log10(abs(f[1:PART_SIZE/2]) ./ PART_SIZE .+ EPSILON)
 		n = (n + 1) % NORM_SIZE
 		if n == 0
 			println(join(result ./ NORM_SIZE, " "))
+			result = Array(Float64, int64(PART_SIZE / 2))
 		end
 	end
 end
